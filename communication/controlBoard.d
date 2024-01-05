6a6a1adfa72e26973d1f71b1d1e4737d129ce33b
Robert Schimpfle <rschimpfle@gmail.com>
2024-01-05 11:59:15 +0100

    initial commit

---
+++ communication/controlBoard.d
@@ -0,0 +1,2 @@
+communication/controlBoard.d communication/controlBoard.o: communication/controlBoard.c communication/controlBoard.h \
+ app/myComm.h
---
+++ communication/module.mk
@@ -0,0 +1,1 @@
+SRC += communication/controlBoard.c
